35408bb7b6c7d5d945b10a7e7692ca9e0559d409
BaayMeissa <meissambaye09@gmail.com>
2024-11-18 15:38:31 +0000

    modification au niveau de app.js, ajout de commentaires

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+
+//Preciser a express, ou il doit aller chercher les fichiers statiques comme style.css, les images etc.
 app.use(express.static('public'))
+
+//Utiliser ejs comme view engine
 app.set('view engine','ejs');
 
 // Vérifie si la requête est effectuée entre lundi et vendredi et entre 9h00 et 17h00
@@ -10,6 +14,7 @@ function myMiddleware(req, res, next) {
   const day = now.getDay();
   const hours = now.getHours();
 
+  //Peut modifier cette partie en manipulant day et hours  pour tester le middleware créé
   if (day >= 1 && day <= 5 && hours >= 9 && hours < 17) {
     next(); // Continue si c'est entre lundi et vendredi, et entre 9h00 et 17h00
   } else {
@@ -18,7 +23,7 @@ function myMiddleware(req, res, next) {
 }
 
 
-// app.use(myMiddleware);
+app.use(myMiddleware);
 
 
 
@@ -37,4 +42,6 @@ app.get('/services',(req,res)=>{
 
 app.listen(port);
 
+
+//Toujours exporter app pour permettre au fichier www de l'utiliser.
 module.exports =app;
